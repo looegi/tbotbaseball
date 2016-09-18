7b3825131f86e6991ffbcd1e6fdd01de71f4a5f6
looegi <lc1512@humboldt.edu>
2016-09-17 21:58:07 -0700

    Update tweetbot.py

--- tweetbot.py
+++ tweetbot.py
@@ -5,11 +5,11 @@ Feed = RssFeeder.Tweet_content
 
 class TwitterAPI:
     def __init__(self):
-        consumer_key = "1Q3LT2y48wZZ48IEm5sX0nx3z"
-        consumer_secret = "5Doyp3ZHoX6Qfc3DpBDfqGY5kqEnrbZ586zkgfRSvIY15oWIuw"
+        consumer_key = ""
+        consumer_secret = ""
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        access_token = "96121325-PGE0KPt6jipztQqr3izWEESCo5itpT5gjO27jJ9B2"
-        access_token_secret = "Qj0S0EcQIaUgG09rXnaCMOM1DOjXMEKzhmIFIERlFf6Q0"
+        access_token = ""
+        access_token_secret = ""
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth)
 
